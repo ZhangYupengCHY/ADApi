4edfeda786f5d3b730008ed6683e54febc33f98f
ramsey16 <1579922399@qq.com>
2021-05-21 11:47:36 +0800

    fix docs ui abnormal and delete test

--- main.py
+++ main.py
@@ -28,6 +28,26 @@ from sql_app.main import walmartAccountRouter,highQualityKwsRouter,databaseRoute
 from extensions import loggers as defaultLoggingConfig
 
 
+#todo 暂时解决文档排版的问题
+
+from fastapi import applications
+from fastapi.openapi.docs import get_swagger_ui_html
+def swagger_monkey_patch(*args, **kwargs):
+    """
+    Wrap the function which is generating the HTML for the /docs endpoint and
+    overwrite the default values for the swagger js and css.
+    """
+    return get_swagger_ui_html(
+        *args, **kwargs,
+        swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.48.0/swagger-ui-bundle.js",
+        swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.48.0/swagger-ui.css"
+    )
+
+
+# Actual monkey patch
+applications.get_swagger_ui_html = swagger_monkey_patch
+
+
 # app的配置项
 def get_application() -> FastAPI:
 
